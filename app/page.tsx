c550e9dea94d212b59d449084efeca683919c220
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2024-08-08 08:05:22 +0900

    💄 3-1-9

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import styles from './page.module.css';
+
 export default function Home() {
   const name = '世界';
 
   return (
     <div>
-      <h1 className="title">テクノロジーの力で{name}を変える</h1>
-      <p className="description">私たちは市場をリードしているグローバルテックカンパニーです。</p>
+      <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+      <p className={styles.description}>
+        私たちは市場をリードしているグローバルテックカンパニーです。
+      </p>
     </div>
   );
 }
