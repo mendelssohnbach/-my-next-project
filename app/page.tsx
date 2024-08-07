9af2fc5bf38d944e08fbc03f29ec5bcbb301a913
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2024-08-07 15:54:20 +0900

    リスト3-1-4

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>テクノロジーの力で世界を変える</h1>;
+  return (
+    <div>
+      <h1>テクノロジーの力で世界を変える</h1>
+      <p>私たちは市場をリードしているグローバルテックカンパニーです。</p>
+    </div>
+  );
 }
