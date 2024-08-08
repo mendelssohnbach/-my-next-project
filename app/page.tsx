132af37548a6c6bb3c5677d7282cbeee70dbb19b
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2024-08-08 12:22:24 +0900

    🚧 3-2-1

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,18 @@ export default function Home() {
   const name = '世界';
 
   return (
-    <div>
-      <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
-      <p className={styles.description}>
-        私たちは市場をリードしているグローバルテックカンパニーです。
-      </p>
-    </div>
+    <section className={styles.top}>
+      <div>
+        <h1 className={styles.title}>テクノロジーの力で{name}を変える</h1>
+        <p className={styles.description}>
+          私たちは市場をリードしているグローバルテックカンパニーです。
+        </p>
+      </div>
+      <img
+        className={styles.bgimg}
+        src="/img-mv.jpg"
+        alt=""
+      />
+    </section>
   );
 }
